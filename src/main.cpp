25af0381e8c12fc52032d24d20667dcedcfff4cf
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2023-07-09 21:42:52 +0200

    remove glm test

--- src/main.cpp
+++ src/main.cpp
@@ -53,14 +53,6 @@ void OnKeyEvent(GLFWwindow *window, int key, int scancode, int action,
 }
 
 int main() {
-  glm::vec3 v1(1.0f, 2.0f, 3.0f);
-  glm::vec3 v2(4.0f, 5.0f, 6.0f);
-
-  glm::vec3 result = v1 + v2;
-
-  std::cout << "Result: (" << result.x << ", " << result.y << ", " << result.z
-            << ")\n";
-
   // Initialize GLFW
   if (!glfwInit()) {
     std::cout << "Failed to initialize GLFW\n";
