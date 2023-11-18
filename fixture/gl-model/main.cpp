923c22dd42cbbeda29d9d84c59326a396dedc5ff
Srecko <srele96@gmail.com>
2023-11-18 21:07:48 +0100

    print some more data

--- fixture/gl-model/main.cpp
+++ fixture/gl-model/main.cpp
@@ -1,6 +1,9 @@
 #include "assimp/Importer.hpp"
+#include "assimp/material.h"
 #include "assimp/postprocess.h"
 #include "assimp/scene.h"
+#include "assimp/types.h"
+#include <array>
 #include <functional>
 #include <iostream>
 
@@ -21,6 +24,7 @@ void traverse(const aiScene *scene, std::ostream &out = std::cout) {
             << " " << mesh->mVertices[j].z << "\n";
       }
 
+      // Draw normals after drawing the model as an exercise.
       // print all normals
       out << "Normals: " << mesh->mNumVertices << "\n";
       for (unsigned int j{0}; j < mesh->mNumVertices; ++j) {
@@ -28,20 +32,76 @@ void traverse(const aiScene *scene, std::ostream &out = std::cout) {
             << " " << mesh->mNormals[j].z << "\n";
       }
 
-      // print all textures
-      out << "Textures: " << mesh->mNumVertices << "\n";
-      for (unsigned int j{0}; j < mesh->mNumVertices; ++j) {
-        out << "Texture: " << mesh->mTextureCoords[0][j].x << " "
-            << mesh->mTextureCoords[0][j].y << "\n";
+      out << "Materials: " << scene->mNumMaterials << "\n";
+
+      // https://stackoverflow.com/questions/8498300/allow-for-range-based-for-with-enum-classes
+      std::array<aiTextureType, 22> texture_types{
+          aiTextureType::aiTextureType_NONE,
+          aiTextureType::aiTextureType_DIFFUSE,
+          aiTextureType::aiTextureType_SPECULAR,
+          aiTextureType::aiTextureType_AMBIENT,
+          aiTextureType::aiTextureType_EMISSIVE,
+          aiTextureType::aiTextureType_HEIGHT,
+          aiTextureType::aiTextureType_NORMALS,
+          aiTextureType::aiTextureType_SHININESS,
+          aiTextureType::aiTextureType_OPACITY,
+          aiTextureType::aiTextureType_DISPLACEMENT,
+          aiTextureType::aiTextureType_LIGHTMAP,
+          aiTextureType::aiTextureType_REFLECTION,
+          aiTextureType::aiTextureType_BASE_COLOR,
+          aiTextureType::aiTextureType_NORMAL_CAMERA,
+          aiTextureType::aiTextureType_EMISSION_COLOR,
+          aiTextureType::aiTextureType_METALNESS,
+          aiTextureType::aiTextureType_DIFFUSE_ROUGHNESS,
+          aiTextureType::aiTextureType_AMBIENT_OCCLUSION,
+          aiTextureType::aiTextureType_SHEEN,
+          aiTextureType::aiTextureType_CLEARCOAT,
+          aiTextureType::aiTextureType_TRANSMISSION,
+          aiTextureType::aiTextureType_UNKNOWN};
+
+      aiMaterial *material{scene->mMaterials[mesh->mMaterialIndex]};
+      out << "Material name: " << material->GetName().C_Str() << "\n";
+      // get materials
+      std::for_each(texture_types.begin(), texture_types.end(),
+                    [&material, &out](aiTextureType texture_type) {
+                      out << "Textures: "
+                          << material->GetTextureCount(texture_type) << "\n";
+                      // All textures are 0. Why?
+                      for (unsigned int j{0};
+                           j < material->GetTextureCount(texture_type); ++j) {
+                        aiString texturePath;
+                        material->GetTexture(texture_type, j, &texturePath);
+                        out << "Texture path: " << texturePath.C_Str() << "\n";
+                      }
+                    });
+
+      aiColor3D diffuseColor;
+      if (material->Get(AI_MATKEY_COLOR_DIFFUSE, diffuseColor) == AI_SUCCESS) {
+        out << "Diffuse color: " << diffuseColor.r << " " << diffuseColor.g
+            << " " << diffuseColor.b << "\n";
+      } else {
+        out << "Diffuse color: not found\n";
+      }
+
+      aiColor3D specularColor;
+      if (material->Get(AI_MATKEY_COLOR_SPECULAR, specularColor) ==
+          AI_SUCCESS) {
+        out << "Specular color: " << specularColor.r << " " << specularColor.g
+            << " " << specularColor.b << "\n";
+      } else {
+        out << "Specular color: not found\n";
       }
 
       // print all indices
-      out << "Indices: " << mesh->mNumFaces << "\n";
+      out << "Faces: " << mesh->mNumFaces << "\n";
       for (unsigned int j{0}; j < mesh->mNumFaces; ++j) {
         aiFace face{mesh->mFaces[j]};
+
+        out << "Face indices: " << face.mNumIndices << "\n";
         for (unsigned int k{0}; k < face.mNumIndices; ++k) {
-          out << "Index: " << face.mIndices[k] << "\n";
+          out << face.mIndices[k] << " ";
         }
+        out << "\n";
       }
     }
 
